ed98ee0948f7efb03b8db2292f696cdacd300ede
Olivia Lind <eo.lind@gmail.com>
2022-06-04 17:14:47 -0500

    updated fetch calls for getAllEpisodes and getEpisodeById to expand on author

--- src/modules/EpisodeManager.js
+++ src/modules/EpisodeManager.js
@@ -2,17 +2,29 @@
 const remoteURL = "http://localhost:8088"
 
 export const getEpisodeById = (episodeId) => {
-  return fetch(`${remoteURL}/episodes/${episodeId}`)
-  .then(res => res.json())
+  return fetch(`${remoteURL}/episodes/${episodeId}?_expand=author`).then(
+      (res) => res.json()
+  )
 }
 
 export const getAllEpisodes = () => {
-  return fetch(`${remoteURL}/episodes`)
-  .then(res => res.json())
+  return fetch(
+      `${remoteURL}/episodes?_sort=id&_order=desc&_expand=author`
+  ).then((res) => res.json())
 }
 
 export const deleteEpisode = (id) => {
     return fetch(`${remoteURL}/episodes/${id}`, {
         method: "DELETE",
     }).then((result) => result.json())
+}
+
+export const addEpisode = (newEpisode) => {
+    return fetch(`${remoteURL}/episodes`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newEpisode),
+    }).then((response) => response.json())
 }
