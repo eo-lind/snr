2fa6cc91fad266d136b7e966618b3f33c0d4ce70
Olivia Lind <eo.lind@gmail.com>
2022-06-04 17:28:11 -0500

    added code for author name with optional chaining (not working at the moment)

--- src/components/episode/EpisodeDetail.js
+++ src/components/episode/EpisodeDetail.js
@@ -36,14 +36,14 @@ export const EpisodeDetail = () => {
             <h3 className="episode__title">{episode.title}</h3>
             <div className="episode__publishDate">{episode.publishDate}</div>
             <div className="episode__author">
-                {/* TODO: */}
-                have to expand fetch call on author id - link to author details
-                page
+                {/* TODO: LATER link to author details page*/}
+                {/* FIXME: This should be displaying an author (all current posts have one) - why isn't it? It breaks if you remove optional chaining operator */}
+                {episode.author?.authorFirst} {episode.author?.authorLast}
             </div>
             <div className="episode__summary">
                 {episode.briefSummaryShowNotes}
             </div>
-            {/* TODO: Implement optional chaining later on the relevant details
+            {/* TODO: Implement optional chaining later on the relevant details (tags... not sure if there will be others)
             https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
              */}
             {/* <div className="episode__location">
